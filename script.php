4781e459fb034daf1ce227a406c30030a7e933b7
ron4mac <ron@rjconline.net>
2024-08-23 14:23:59 +0000

    remove old J3-type main module php file

---
+++ script.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* @package		mod_rjcweather
+* @copyright	Copyright (C) 2015-2024 RJCreations. All rights reserved.
+* @license		GNU General Public License version 3 or later; see LICENSE.txt
+* @since		1.2.0
+*/
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Installer\InstallerScript;
+
+class mod_rjcweatherInstallerScript extends InstallerScript
+{
+	protected $minimumJoomla = '4.0';
+	protected $deleteFiles = ['/modules/mod_rjcweather/mod_rjcweather.php'];
+
+	public function install ($parent) 
+	{
+	}
+
+	public function uninstall ($parent) 
+	{
+	}
+
+	public function update ($parent) 
+	{
+	}
+
+	public function preflight ($type, $parent) 
+	{
+	}
+
+	public function postflight ($type, $parent) 
+	{
+		if ($type === 'update') {
+			$this->removeFiles();
+		}
+		return true;
+	}
+
+}
