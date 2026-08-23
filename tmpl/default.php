304d03fdae8ff25f53d30dd6d250e234c60cc74d
ron4mac <ron@rjconline.net>
2026-08-23 13:11:22 +0000

    a couple more small tweaks

--- tmpl/default.php
+++ tmpl/default.php
@@ -12,24 +12,22 @@ use Joomla\Registry\Registry;
 
 $wa = Factory::getDocument()->getWebAssetManager();
 
-$wa->registerAndUseStyle('rjcw-style', 'media/mod_rjcweather/css/weather.css');
-$wa->registerAndUseScript('rjcw-script', 'media/mod_rjcweather/js/weather.js');
+$wa->registerAndUseStyle('rjcw-style', 'mod_rjcweather/weather.css');
+$wa->registerAndUseScript('rjcw-script', 'mod_rjcweather/weather.js');
 
 $params = new Registry($module->params);
 
 $moduleID = $module->id;
+// no sense in refreshing the display until cache has expired
 $checkDly = ($params->get('cache_time', 5)+1) * 60000;
 
-$js = <<<JS
-window.addEventListener('load', () => {
-	mod_rjcw({$moduleID});
-	const timerId = setInterval(() => {mod_rjcw({$moduleID});}, {$checkDly});
-	return false;
-});
+$jsd = <<<JS
+mod_rjcw({$moduleID});
+setInterval(() => mod_rjcw({$moduleID}), {$checkDly});
 JS;
 
-// add javascript to document head
-$wa->addInlineScript($js);
+// add javascript to document head .. declare as module so execution gets defered
+$wa->addInlineScript($jsd, [], ['type' => 'module']);
 
 ?>
 <div id="rjc_weather_id<?=$moduleID?>">
